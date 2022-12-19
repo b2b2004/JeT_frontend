8b67e53e14e76891b1e5b187010fe3b2daba6645
Kwon yongho <b2b2004@nate.com>
2022-12-19 15:46:57 +0900

    ex

--- src/component/modal/SecondModal.js
+++ src/component/modal/SecondModal.js
@@ -2,9 +2,41 @@ import Calendar from "react-calendar";
 import moment from "moment";
 import {nextStep, previousStep, set_duration_end, set_duration_start} from "../../store/RecommendCourseStep";
 import {useDispatch} from "react-redux";
+import Select from "react-select";
 
 const SecondModal = ({handleClose}) =>{
     const dispatch = useDispatch();
+    const Checkbox = new Set();
+
+    const changeSelect = (e) => {
+        let length = e.length;
+
+        if(Checkbox.has(e[length-1].value) === false){
+            Checkbox.add(e[length-1].value);
+        }else if (Checkbox.has(e[length-1].value) === true)
+        {
+            Checkbox.delete(e[length-1].value);
+        }
+
+        const test = [...Checkbox];
+        const techStack = test.join(',');
+       // dispatch(setTechStack(techStack));
+    }
+
+    const options = [
+        {id:1, value: "가", label: "가"},
+        {id:2, value: "나", label: "나"},
+        {id:3, value: "다", label: "다"},
+        {id:4, value: "라", label: "라"},
+        {id:5, value: "마", label: "마"},
+        {id:6, value: "바", label: "바"},
+        {id:7, value: "사", label: "사"},
+        {id:8, value: "아", label: "아"},
+        {id:9, value: "자", label: "자"},
+        {id:10, value: "차", label: "차"},
+        {id:11, value: "카", label: "카"},
+        {id:12, value: "타", label: "타"},
+    ]
 
     const nexthandleStep = async () => {
         dispatch(nextStep());
@@ -15,7 +47,16 @@ const SecondModal = ({handleClose}) =>{
 
     return  (
         <>
-            + 키워드 / 사용자 성향
+            <h1>키워드를 선택해주세요</h1>
+            <Select
+                isMulti
+                name="area"
+                onChange={changeSelect}
+                className="languages"
+                options={options}
+                placeholder="기술을 선택해주세요."
+            />
+
             <footer className="modal_footer">
                 <img
                     className="arrow-left-title"
--- src/component/modal/ThirdModal.js
+++ src/component/modal/ThirdModal.js
@@ -1,10 +1,14 @@
 import Calendar from "react-calendar";
 import {useDispatch} from "react-redux";
 import {nextStep, previousStep} from "../../store/RecommendCourseStep";
+import {useEffect, useState} from "react";
+import Card from "../card/Card";
 
 const ThirdModal = ({handleClose}) =>{
 
     const dispatch = useDispatch();
+    const [jejuData, setJejuData] = useState([]);
+    let [pageNo,setPageNo] = useState(6);
     const nexthandleStep = async () => {
         dispatch(nextStep());
     };
@@ -12,9 +16,30 @@ const ThirdModal = ({handleClose}) =>{
         dispatch(previousStep());
     };
 
+    useEffect(()=>{
+        fetch("http://localhost:8087/board/areaTravel",
+            {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                },
+                body: JSON.stringify(pageNo)
+            })
+            .then((res)=> res.json())
+            .then((res) =>{
+                console.log(res);
+                setJejuData(res);
+            })
+    },[])
+
     return  (
         <>
-            + 지도인기 관광지
+            <div className='cards'>
+                {jejuData.map((jejuData) => (
+                    <Card key={jejuData.JejuDataNo} jejuData={jejuData} />
+                ))}
+            </div>
+
             <footer className="modal_footer">
                 <img
                     className="arrow-left-title"
